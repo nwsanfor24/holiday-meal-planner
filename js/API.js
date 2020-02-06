f3ff095d9a14f0053abc75218c04cba86b3abcf0
Brendan Gallagher <bpgallagher27@gmail.com>
2020-02-05 19:24:33 -0500

    initial API js setup

--- js/API.js
+++ js/API.js
@@ -1,1 +1,21 @@
+$(document).ready(function() {
 
+    let drinkURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php?a=Alcoholic";
+    let mealURL = "https://www.themealdb.com/api/json/v1/1/random.php";
+
+    $.ajax({
+        url: drinkURL,
+        method: "GET",
+    }).then(function(updateDrink) {
+        $(".cocktail-title").text(updateDrink.drinks[strDrink]);
+        $(".cocktail-img").attr("src", updateDrink.drinks[strDrinkThumb]);
+    });
+
+
+
+
+
+
+
+
+});
