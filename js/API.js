cf425538f8f549b182a4f97169d2c70a16ab4821
Brendan Gallagher <bpgallagher27@gmail.com>
2020-02-05 19:55:41 -0500

    first drinks API

--- js/API.js
+++ js/API.js
@@ -7,8 +7,15 @@ $(document).ready(function() {
         url: drinkURL,
         method: "GET",
     }).then(function(updateDrink) {
-        $(".cocktail-title").text(updateDrink.drinks[strDrink]);
-        $(".cocktail-img").attr("src", updateDrink.drinks[strDrinkThumb]);
+        $(".cocktail-title").text(updateDrink.drinks[0].strDrink);
+        $(".cocktail-img").attr("src", updateDrink.drinks[0].strDrinkThumb);
+        $(".cocktail-description").text(updateDrink.drinks[0].strGlass);
+        $(".cocktail-steps").text(updateDrink.drinks[0].strInstructions);
+        let drinkIngredients = updateDrink.drinks[0].strIngredient;
+        for(i=1; i < drinkIngredients.length; i++) {
+            let newIngredient = $("<li>").text(drinkIngredients[i]);
+            $(".cocktail-ingredients").append(newIngredient);
+        };
     });
 
 
