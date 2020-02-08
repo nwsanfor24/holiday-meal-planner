c3a512e601e444cbd9d2dc618bdf3882ce5ddae1
Brendan Gallagher <bpgallagher27@gmail.com>
2020-02-08 11:05:05 -0500

    cocktail comments for API js

--- js/API.js
+++ js/API.js
@@ -1,19 +1,23 @@
 
-
+//Declaring the two API URLs
 let drinkInfoURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 let mealURL = "https://www.themealdb.com/api/json/v1/1/random.php";
 
+//Function to wrap button click functionality for the cocktail field
 function updateDrink() { 
        
+    //Cocktal API call
     $.ajax({
         url: drinkInfoURL,
         method: "GET",
+    //Initial response sets some cocktail box information
     }).then(function (response) {
         $(".cocktail-title").text(response.drinks[0].strDrink);
         $(".cocktail-img").attr("src", response.drinks[0].strDrinkThumb);
         $(".cocktail-description").text(response.drinks[0].strGlass);
         $(".cocktail-steps").text(response.drinks[0].strInstructions);
 
+        //Pulling measurement amounts for cocktail ingredients from API
         let drinkAmounts = [
             response.drinks[0].strMeasure1,
             response.drinks[0].strMeasure2,
@@ -32,6 +36,7 @@ function updateDrink() {
             response.drinks[0].strMeasure15,
         ];
 
+        //Pulling cocktail ingredients from API
         let drinkIngredients = [
             response.drinks[0].strIngredient1,
             response.drinks[0].strIngredient2,
@@ -50,21 +55,22 @@ function updateDrink() {
             response.drinks[0].strIngredient15,
         ];
 
+        //Loop set up to post ingredients and amounts to the page
         for (i = 0; i < drinkIngredients.length; i++) {
             let postedDrinkAmounts = drinkAmounts[i];
             let postedDrinkIngredients = drinkIngredients[i];
             let newDrink = $("<li>").text(`${postedDrinkAmounts} ${postedDrinkIngredients}`);
+            
+            //funtcion to filter out empty ingredient and amount fields
             if (drinkAmounts[i] !== null) {
                 let newDrink = $("<li>").text(`${postedDrinkIngredients}`);
                 $(".cocktail-ingredients").append(newDrink);
-            }
+                }
             if (drinkIngredients[i] !== null) {
                 $(".cocktail-ingredients").append(newDrink);
-            }
+                }
             };
 
     });
-
-
-   };
+};
 
